caf2b831e04f6e143c810f0d422a94bffefc874e
刘禹辰 <yuchen@10-209-123-10.dynapool.shanghai.nyu.edu>
2020-09-10 15:47:19 +0800

    project1 finished

--- p1/SDLProject/SDLProject/main.cpp
+++ p1/SDLProject/SDLProject/main.cpp
@@ -7,13 +7,45 @@
 #define GL_GLEXT_PROTOTYPES 1
 #include <SDL.h>
 #include <SDL_opengl.h>
+#include "glm/mat4x4.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "ShaderProgram.h"
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 
 SDL_Window* displayWindow;
 bool gameIsRunning = true;
 
-int main(int argc, char* argv[]) {
+ShaderProgram program;
+glm::mat4 viewMatrix, birdMatrix, personMatrix, projectionMatrix;
+GLuint playerTextureID, birdTextureID;
+float bird_x = 0.0f;
+float bird_rot = 0.0f;
+
+float player_x = 0.0f;
+
+
+GLuint LoadTexture(const char* filePath) {
+    int w, h, n;
+    unsigned char* image = stbi_load(filePath, &w, &h, &n, STBI_rgb_alpha);
+    if (image == NULL) {
+        std::cout << "Unable to load image. Make sure the path is correct\n";
+        assert(false);
+    }
+    GLuint textureID;
+    glGenTextures(1, &textureID);
+    glBindTexture(GL_TEXTURE_2D, textureID);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    stbi_image_free(image);
+    return textureID;
+}
+
+void Initialize() {
     SDL_Init(SDL_INIT_VIDEO);
-    displayWindow = SDL_CreateWindow("Hello, World!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_OPENGL);
+    displayWindow = SDL_CreateWindow("Project1!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_OPENGL);
     SDL_GLContext context = SDL_GL_CreateContext(displayWindow);
     SDL_GL_MakeCurrent(displayWindow, context);
     
@@ -21,20 +53,91 @@ int main(int argc, char* argv[]) {
     glewInit();
 #endif
     
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glViewport(0, 0, 1280, 960);
     
+    program.Load("shaders/vertex_textured.glsl", "shaders/fragment_textured.glsl");
+    
+    viewMatrix = glm::mat4(1.0f);
+    birdMatrix = glm::mat4(1.0f);
+    personMatrix = glm::mat4(1.0f);
+    projectionMatrix = glm::ortho(-5.0f, 5.0f, -3.75f, 3.75f, -1.0f, 1.0f);
+    
+    program.SetProjectionMatrix(projectionMatrix);
+    program.SetViewMatrix(viewMatrix);
+    //program.SetColor(1.0f, 0.0f, 0.0f, 1.0f);
+    
+    glUseProgram(program.programID);
+    
+    glClearColor(0.5f, 0.2f, 0.2f, 1.0f);
+    
+    playerTextureID = LoadTexture("ctg.png");
+    birdTextureID = LoadTexture("angrybird.png");
+
+    glEnable(GL_BLEND);
+    // Good setting for transparency
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+     
+}
+
+void ProcessInput() {
     SDL_Event event;
-    while (gameIsRunning) {
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT || event.type == SDL_WINDOWEVENT_CLOSE) {
-                gameIsRunning = false;
-            }
+    while (SDL_PollEvent(&event)) {
+        if (event.type == SDL_QUIT || event.type == SDL_WINDOWEVENT_CLOSE) {
+            gameIsRunning = false;
         }
-        
-        glClear(GL_COLOR_BUFFER_BIT);
-        SDL_GL_SwapWindow(displayWindow);
+    }
+}
+
+
+float lastTicks = 0.0f;
+void Update() {
+    birdMatrix = glm::mat4(1.0f);
+    float ticks = (float)SDL_GetTicks() / 1000.0f;
+    float deltaTime = ticks - lastTicks;
+    lastTicks = ticks;
+    bird_x += 1.0f * deltaTime;
+    bird_rot += 90.0f * deltaTime;
+    
+    birdMatrix = glm::rotate(birdMatrix,glm::radians(bird_rot), glm::vec3(0,0,-1));
+    birdMatrix = glm::translate(birdMatrix, glm::vec3(bird_x,0,0));
+    
+    player_x += 1.0f * deltaTime;
+    personMatrix = glm::mat4(1.0f);
+    personMatrix = glm::translate(personMatrix, glm::vec3(player_x,0,0));
+}
+
+void Render() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    program.SetModelMatrix(personMatrix);
+    float vertices[] = { -0.5, -0.5, 0.5, -0.5, 0.5, 0.5, -0.5, -0.5, 0.5, 0.5, -0.5, 0.5 };
+    float texCoords[] = { 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0 };
+    glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertices);
+    glEnableVertexAttribArray(program.positionAttribute);
+    glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, texCoords);
+    glEnableVertexAttribArray(program.texCoordAttribute);
+    glBindTexture(GL_TEXTURE_2D, playerTextureID);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+    program.SetModelMatrix(birdMatrix);
+    glBindTexture(GL_TEXTURE_2D, birdTextureID);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+    glDisableVertexAttribArray(program.positionAttribute);
+    glDisableVertexAttribArray(program.texCoordAttribute);
+    SDL_GL_SwapWindow(displayWindow);
+}
+
+void Shutdown() {
+    SDL_Quit();
+}
+
+int main(int argc, char* argv[]) {
+    Initialize();
+    
+    while (gameIsRunning) {
+        ProcessInput();
+        Update();
+        Render();
     }
     
-    SDL_Quit();
+    Shutdown();
     return 0;
 }
